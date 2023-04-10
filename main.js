96fbead246146b87a1d5b8b5763deae39b4356ef
safiyahl <safiyah.seedat@gmail.com>
2023-04-10 11:45:45 +0100

    kata and tests

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-/* 
-👉 Write your kata here!
-*/
 
-//👉 Write the function your CodeWarriors will start with below here:
+//👉 Write your kata here!
+// This kata creates a function that calculates the number of days between two dates
+// There is a celebration coming up and we want a countdown to it.
+// The console.log should print out the number of days left until the celebration.
+
+// HINT; the function uses maths.abs and math.floor
+
+function getDaysBetweenDates(date1, date2) 
+// write your code here 
+    getDaysBetweenDates(today, celebrationDate);
+
+console.log("There are", daysBetween ," days left until Eid!");
--- main.test.js
+++ main.test.js
@@ -1,1 +1,26 @@
-//👉 Write your tests below here:
+import {test, expect} from "@jest/globals";
+import {getDaysBetweenDates} from "./main.js";
+
+const moment = require('moment');
+
+
+test("getDaysBetweenDates function exists", () => {
+  expect(getDaysBetweenDates).toBeDefined();
+});
+// Ensure the function returns the correct number of days between two dates
+test("getDaysBetweenDates function returns the number of days between two dates", () => {
+  expect(getDaysBetweenDates("2023-04-09", "2023-04-21")).toBe(12);
+});
+
+//write a test that expects todays date to be in the correct formal (yyyy-mm-dd)
+test('Check if date is in YYYY-MM-DD format', () => {
+    const date = '2023-04-09';
+    const expectedFormat = 'YYYY-MM-DD';
+    // Use moment.js to parse the date string and format it
+    const formattedDate = moment(date, expectedFormat).format(expectedFormat);
+    // Use Jest's expect function to make assertions
+    expect(formattedDate).toBe(date);
+  });
+  
+
+
